50c307f642f9b42135fbf92e6c37fed9b8a706ad
Mohamed reda <mohamedreda26499@gmail.com>
2024-10-02 19:33:15 +0200

    -- added new pagination to the view.

--- books/views.py
+++ books/views.py
@@ -2,12 +2,21 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+from utils.CustomPageNumberPagination import CustomPageNumberPagination
 from .models import Book
 from .serializers import BookSerializer
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 
-@extend_schema(tags=['Books'])
+
+@extend_schema(
+    tags=['Books'],
+    parameters=[
+        OpenApiParameter(name='page', description='Page number for pagination', required=False, type=int),
+        OpenApiParameter(name='page_size', description='Number of results per page (max: 100)', required=False, type=int)
+    ]
+)
 class BookViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = CustomPageNumberPagination
