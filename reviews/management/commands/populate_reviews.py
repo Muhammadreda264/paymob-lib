8ee3ddb153fbf3964df70d51a5924ef9badfd6dd
Mohamed reda <mohamedreda26499@gmail.com>
2024-10-02 19:30:32 +0200

    -- updated populate_reviews.py to follow the new review model.

--- reviews/management/commands/populate_reviews.py
+++ reviews/management/commands/populate_reviews.py
@@ -1,5 +1,3 @@
-# reviews/management/commands/populate_reviews.py
-
 import random
 from django.core.management.base import BaseCommand
 from faker import Faker
@@ -10,12 +8,24 @@ from books.models import Book
 class Command(BaseCommand):
     help = 'Populate the Review model with fake data'
 
+    def add_arguments(self, parser):
+        # Adding an optional argument for the number of reviews
+        parser.add_argument(
+            '--num-reviews',
+            type=int,
+            default=10,
+            help='Number of fake reviews to create (default is 10)',
+        )
+
     def handle(self, *args, **kwargs):
         fake = Faker()
         users = User.objects.all()  # Fetch all users to assign as reviewers
         books = Book.objects.all()  # Fetch all books to assign reviews
-        #TODO: Make ths a param to the script
-        for _ in range(10):
+
+        # Get the number of reviews from the command-line argument
+        num_reviews = kwargs['num_reviews']
+
+        for _ in range(num_reviews):
             review = Review(
                 book=random.choice(books),  # Randomly select a book
                 reviewer=random.choice(users),  # Randomly select a user as reviewer
