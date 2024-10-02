b4c9d09b0639a996b0d57c752c36b1ac85cb2a42
Mohamed reda <mohamedreda26499@gmail.com>
2024-10-02 19:29:16 +0200

    -- updated rating to be a number between 1 and 5 . -- added a Constraint to ensure user can only review a book once.

--- reviews/models.py
+++ reviews/models.py
@@ -1,13 +1,21 @@
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from books.models import Book  # Import Book model
 
 class Review(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
     reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]  # Restrict to values between 1 and 5
+    )
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['book', 'reviewer'], name='unique_book_reviewer')
+        ]
 
     def __str__(self):
         return f"Review by {self.reviewer} on {self.book.title}"
