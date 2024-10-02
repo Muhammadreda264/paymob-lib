2843c94522a6ac15361e7764ccb481f288a57d10
Mohamed reda <mohamedreda26499@gmail.com>
2024-10-02 16:19:35 +0200

    -- update urls.py to add all needed endpoints.

--- cool_book_store/urls.py
+++ cool_book_store/urls.py
@@ -1,8 +1,26 @@
+from django.contrib import admin
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from books.views import BookViewSet
-
+from reviews.views import ReviewViewSet
+from jwt_auth.views import RegisterView, CustomTokenObtainPairView, CustomTokenRefreshView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 # Initialize the DefaultRouter
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book')
+router.register(r'reviews', ReviewViewSet, basename='review')
 
+urlpatterns = [
+    path('api/books/<int:book_id>/reviews/', ReviewViewSet.as_view({'get': 'reviews_for_book_by_id'}), name='book-reviews'),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/auth/register/', RegisterView.as_view(), name='register'),
+    path('api/auth/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+
+    # Spectacular API and documentation URLs
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
